9d5626db790fa6b3ee3da1d444d00acf8eaa04d5
szogics <szogimate@gmail.com>
2017-09-26 12:27:34 +0200

    Make the file. Create matrix.

---
+++ src/generateMineField.js
@@ -0,0 +1,12 @@
+function generateMineField(width, height, mineCount){
+
+    var M;
+    var current
+    for(var i=0; i<width; i++)
+    {
+        for(var j=0; j<height;j++)
+        {
+            M[i,j]=0;
+        }
+    }
+}
