f38adf224de70fe8cbf2e96620f39fc55057d97d
fmarkyy98 <fmark98@gmail.com>
2017-09-26 12:32:42 +0200

    5. feladat függvénye létrehozva, logika leírása kommentben.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 console.log('hello');
+
+function whatIsGameState(mF, vF){
+
+}
+// ha van mine a filden ahol visible = true: -1
+//ha nincs sima / számos mező shol visible = false: 1
+//egyébként: 0
