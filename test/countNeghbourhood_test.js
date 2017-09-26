63e87597a5e42e8b235d24af9c9f595740b4ef86
IDLCN <imre.david.levente@gmail.com>
2017-09-26 13:41:14 +0200

    countNeighbourhood

---
+++ test/countNeghbourhood_test.js
@@ -0,0 +1,36 @@
+var expect=require('chai').expect;
+var TBD=require("../src/countNeighbourhood").TBD;
+
+describe("MsTest",function () {
+    it("2by2",function () {
+        var result=TBD([[0,-1],[0,0]]);
+        expect(result).to.be.deep.equal([[1,-1],[1,1]]);
+    });
+    it("[]",function () {
+        var result=TBD([]);
+        expect(result).to.be.deep.equal([]);
+    });
+    it("large",function () {
+        var result=TBD([[0,-1,0,-1],[0,0,0,0],[-1,-1,-1,-1],[-1,0,0,-1]]);
+        expect(result).to.be.deep.equal([[1,-1,2,-1],[3,4,5,3],[-1,-1,-1,-1],[-1,4,4,-1]]);
+    });it("bombs",function () {
+        var result=TBD([[-1,-1],[-1,-1]]);
+        expect(result).to.be.deep.equal([[-1,-1],[-1,-1]]);
+    });
+    it("3by1",function () {
+        var result=TBD([[0,-1,0]]);
+        expect(result).to.be.deep.equal([[1,-1,1]]);
+    });
+    it("1by3",function () {
+        var result=TBD([[0],[-1],[0]]);
+        expect(result).to.be.deep.equal([[1],[-1],[1]]);
+    });
+    it("2by4",function () {
+        var result=TBD([[-1,0,0,0],[-1,0,0,-1]]);
+        expect(result).to.be.deep.equal([[-1,2,1,1],[-1,2,1,-1]]);
+    });
+    it("3byvarious",function () {
+        var result=TBD([[0,-1,0],[0],[-1,0]]);
+        expect(result).to.be.deep.equal([[1,-1,1],[2],[-1,1]]);
+    });
+} );
