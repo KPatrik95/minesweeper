d7b48ca1853fc4f94a661835bb45300cf283ee6a
szogics <szogimate@gmail.com>
2017-09-26 12:41:12 +0200

    Alpha v.1.0.0

--- src/generateMineField.js
+++ src/generateMineField.js
@@ -5,7 +5,18 @@ function generateMineField(width, height, mineCount){
     {
         for(var j=0; j<height;j++)
         {
-            M[i,j]=0;
+            M[i][j]=0;
+        }
+    }
+    var currentMines=0;
+    while(currentMines<mineCount)
+    {
+        var i=Math.floor((Math.random()*width)+1);
+        var j=Math.floor((Math.random()*height)+1);
+        if(M[i,j]==0)
+        {
+            M[i,j]=-1;
+            currentMines++;
         }
     }
 }
