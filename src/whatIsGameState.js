2e4f9e57973ca157c68bdc6a631f791159474bee
fmarkyy98 <fmark98@gmail.com>
2017-09-26 12:48:09 +0200

    Kölön File létrehozása.

--- src/whatIsGameState.js
+++ src/whatIsGameState.js
@@ -1,6 +1,17 @@
-function whatIsGameState(mF, vF){
 
+
+
+
+
+
+
+function whatIsGameState(mF, vF) {
+    for (var i = 0; i < mF.length; i++) {
+        for (var j = 0; j < mF[i].length; j++) {
+            return mF[i][j] == -1 && vF[i][j] ? -1 : mF[i][j] != -1 && !vF[i][j] ? 0 : 1;
+        }
+    }
 }
 // ha van mine a filden ahol visible = true: -1
-//ha nincs sima / számos mező shol visible = false: 1
+//ha nincs sima mező shol visible = false: 1
 //egyébként: 0
