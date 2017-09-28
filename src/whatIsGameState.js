894e9e322de0b9a1abd3d788cb7ed34fd5b323e3
fmarkyy98 <fmark98@gmail.com>
2017-09-28 14:22:06 +0200

    bugfix again

--- src/whatIsGameState.js
+++ src/whatIsGameState.js
@@ -4,7 +4,6 @@ function whatIsGameState(mineField, visibleField) {
         for (var j = 0; j < mineField[i].length; j++) {
             if (mineField[i][j] == -1 && visibleField[i][j]) {
                 return -1;
-                continue;
             }
             if (mineField[i][j] != -1 && !visibleField[i][j]) {
                 value = 0;
