fe06a9dcd69d8e7ff9866832e513dc368232e723
fmarkyy98 <fmark98@gmail.com>
2017-10-03 13:29:09 +0200

    BombSzámlálo Try 1 .

---
+++ test/reamaindMineCount_test.js
@@ -0,0 +1,17 @@
+var expect=require('chai').expect;
+var remaindMineCount = require('../src/remaindMineCount').remaindMinceCount;
+
+describe ("RemaindMineCount",function () {
+    it("Teszt 1",function () {
+        var result = remaindMineCount([
+            [true, false, false],
+            [false, true, false],
+            [true, true, false]
+        ]);
+
+        var mineCount = 2;
+        expect(result).to.be.equal(-2);
+
+    });
+
+});
